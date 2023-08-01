b55eb88e94884089be581ad651c81fda6cbff4d3
Mike-chege <mikechege93@gmail.com>
2023-08-01 05:10:52 -0700

    Used malloc or memory allocation if it fails returns NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,13 +11,18 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
 	if (parent == NULL)
+	{
 		return (NULL);
-
+	}
+	
+	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
+	{
 		return (NULL);
+	}
 
 	node->n = value;
 	node->left = NULL;
