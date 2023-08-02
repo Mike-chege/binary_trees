e655fa26d619dc74d7bea32a98f78c7077b86918
Mike-chege <mikechege93@gmail.com>
2023-08-02 06:52:29 -0700

    Getting the depth of a binary tree node recursively

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,5 +14,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	size += (tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0;
-	return(size);
+	return (size);
 }
