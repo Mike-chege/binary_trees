1df62688c1f3aceb95839ac7c23fc609ef535b6d
Mike-chege <mikechege93@gmail.com>
2023-08-03 10:43:45 -0700

    checking if the binary tree is perfect/fixed typos

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -5,6 +5,7 @@ size_t binary_tree_hgt(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_check_full(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_perfect - Checks if binary tree is perfect
